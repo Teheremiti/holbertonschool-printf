b98a4e2f3226aa1256cf4f17a9506ea124a0531d
Teheremiti <t.tuiaiho@gmail.com>
2023-03-27 06:07:57 -0700

    First version of _printf

--- _printf.c
+++ _printf.c
@@ -10,17 +10,42 @@
 
 int _printf(const char *format, ...)
 {
+	print_t spec[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
+	int i, j;
+	int count = 0;
+
 	va_list ap;
-	int i = 0;
 
 	va_start(ap, format);
 
+	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
+		if (format[i] != '%')
+		{
+			char chr = format[i];
+
+			count += write(1, &chr, 1);
+		}
+
+		j = 0;
+		while (spec[j].type != '\0')
+		{
+			if (format[i] == '%' && format[i + 1] == spec[j].type)
+			{
+				count += spec[j].f(ap);
+				i++;
+			}
+			j++;
+		}
 		i++;
 	}
 
 	va_end(ap);
-
-	return (0);
+	return (count);
 }
