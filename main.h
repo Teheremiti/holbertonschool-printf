0eba93a2078ccca8261c1af2b705b8c034968db5
Teheremiti <t.tuiaiho@gmail.com>
2023-03-27 06:06:29 -0700

    Add prototypes + Include guard

--- main.h
+++ main.h
@@ -1,5 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <unistd.h>
 #include <stdarg.h>
+#include <string.h>
 
 typedef struct print_format
 {
@@ -7,4 +11,9 @@ typedef struct print_format
 	void (*f)(va_list);
 } print_t;
 
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
 int _printf(const char *format, ...);
+
+#endif
