5035fb02a11a2fde6361791b8ac174ee8f935d8e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-29 01:26:23 -0700

    corrected comments on first three functions and added additional two functions: print_int and rev_string functions to process %d specifier

--- print_func.c
+++ print_func.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_char - Print a char to stdout
+ * print_char - Prints a char to stdout
  * @ap: Argument parameters
+ *
  * Return: Number of characters printed
  */
 int print_char(va_list ap)
@@ -13,8 +14,9 @@ int print_char(va_list ap)
 }
 
 /**
- * print_string - Print a string to stdout
+ * print_string - Prints a string to stdout
  * @ap: Argument parameters
+ *
  * Return: Number of characters printed
  */
 int print_string(va_list ap)
@@ -29,8 +31,9 @@ int print_string(va_list ap)
 }
 
 /**
- * print_percent - Print the percent character to stdout
+ * print_percent - Prints the percent character to stdout
  * @ap: Argument parameters
+ *
  * Return: Number of characters printed
  */
 int print_percent(va_list ap)
@@ -40,3 +43,83 @@ int print_percent(va_list ap)
 
 	return (write(1, &c, 1));
 }
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0, len = strlen(s), tmp;
+
+	if (len % 2)
+	{
+		for (; i <= len / 2; i++)
+		{
+			tmp = s[i];
+			s[i] = s[len - 1 - i];
+			s[len - 1 - i] = tmp;
+		}
+	}
+	else
+	{
+		for (; i <= len / 2 - 1; i++)
+		{
+			tmp = s[i];
+			s[i] = s[len - 1 - i];
+			s[len - 1 - i] = tmp;
+		}
+	}
+}
+
+/**
+ * print_int - Prints an integer
+ * @ap: Argument parameters
+ *
+ * Return: Number of characters printed
+ */
+int print_int(va_list ap)
+{
+	int len = 0, i = 0;
+	char zero;
+	int num = va_arg(ap, int);
+	int numCopy = num;
+	char *strNum = NULL;
+
+	if (num == 0)
+	{
+		zero = '0';
+		return (write(1, &zero, 1));
+	}
+
+	while (numCopy != 0)
+	{
+		len++;
+		numCopy = numCopy / 10;
+	}
+
+	if (num > 0)
+		strNum = malloc(sizeof(strNum) * len + 1);
+	else if (num < 0)
+		strNum = malloc(sizeof(strNum) * len + 2);
+
+	if (num < 0)
+	{
+		num = -1 * num;
+		strNum[len] = '-';
+	}
+
+	while (num != 0)
+	{
+		strNum[i] = num % 10 + '0';
+		i++;
+		num = num / 10;
+	}
+
+	rev_string(strNum);
+	return (write(1, strNum, strlen(strNum)));
+}
+
+
